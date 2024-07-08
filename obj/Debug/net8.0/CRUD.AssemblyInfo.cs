aa986b2ab70d9e49ddabe01b45ab85154cb94d32
guelo0987 <jessielopez0987@gmail.com>
2024-07-08 12:38:25 -0400

    UpdatedConnectionstring

--- obj/Debug/net8.0/CRUD.AssemblyInfo.cs
+++ obj/Debug/net8.0/CRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e8bf8bfb959f5cd2b6b63b4a5ae14abafd69a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050ea540cf0a6076b3f05ada5890a0d0194f81f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
